52d43af4730215c542fb68d08628ec924a0cf75b
afhpayne <github@komputermatrix.com>
2021-05-30 21:14:17 -0400

    update

--- slackware/first_boot_after_kernel.sh
+++ slackware/first_boot_after_kernel.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+sudo_user=$SUDO_USER
 gpu_check=$(command -v nvidia-smi)
 if [ -z $gpu_check ]
 then
     printf "nvidia not found...\n"
 else
     printf "nvidia found... $gpu_check \n"
-    sh /home/$SUDO_USER/Github/linuxscripts/slackware/nvidia_expect.sh
+    expect /home/$SUDO_USER/Github/linuxscripts/slackware/nvidia_expect.exp $sudo_user
 fi
 
 sh /home/$SUDO_USER/Nextcloud/Installers/VirtualBox*.run \
@@ -18,3 +19,4 @@ flatpak remove --unused \
 reboot
 
 exit 1
+
